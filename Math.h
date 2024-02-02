34337a6377e4ba8edc1d5fded8eb56739bfeb200
kairi2371 <k022g0109@g.neec.ac.jp>
2024-02-02 12:26:04 +0900

    2-2

---
+++ Math.h
@@ -0,0 +1,35 @@
+#pragma once
+
+template <typename Type1,typename Type2>
+
+class MT
+{
+public:
+
+	/*MT(Type1 a, Type1 b) {
+		num1 = a, num2 = b;
+	}*/
+
+	//è„Ç∆ìØÇ∂à”ñ°
+	MT(Type1 num1, Type2 num2) :num1(num1), num2(num2) {};
+
+
+	Type1 Min() {
+		if (num1 < num2) {
+			return static_cast<Type1>(num1);
+		}
+
+		if (num1 > num2) {
+			return static_cast<Type1>(num2);
+		}
+	}
+
+
+private:
+
+	Type1 num1;
+	Type2 num2;
+
+};
+
+
--- main.cpp
+++ main.cpp
@@ -1,14 +1,19 @@
 #include <stdio.h>
-#include "Enemy.h"
+#include "Math.h"
 
 int main() {
 
-	Enemy* enemy_ = new Enemy;
+	MT<int, int>num1(3, 5);
+	MT<float, float>num2(1.5f, 2.1f);
+	MT<double, double>num3(2.5f, 5.1f);
+	MT<int, float>num4(5, 4.8f);
+	MT<int, double>num5(6,7.9f);
+	MT<float, double>num6(5.4f, 7.5f);
 
-	while (enemy_->GetCount() == false) {
-		enemy_->Update();
-	}
-
-
-	return 0;
+	printf("%d\n", num1.Min());
+	printf("%f\n", num2.Min());
+	printf("%f\n", num3.Min());
+	printf("%d\n", num4.Min());
+	printf("%d\n", num5.Min());
+	printf("%f\n", num6.Min());
 }
